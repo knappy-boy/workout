985a1b7ad9be1c73f247df16fe723de7425fd54e
knappy-boy <alexanderknapper@googlemail.com>
2026-01-02 21:19:42 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "liftlog-v1";
+const CACHE = "liftlog-v2";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
