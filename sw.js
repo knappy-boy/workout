a3846e7f66f7c354073e6548420d2d856c2fa245
knappy-boy <alexanderknapper@googlemail.com>
2026-01-02 20:28:42 +0000

    sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "liftlog-v1";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match("./")))
+  );
+});
